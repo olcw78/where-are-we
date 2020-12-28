4fbbe7ae1c45886cdfd9a3a7821b98c247592484
yoonsang Lee <dlrnt1995@gmail.com>
2020-12-29 00:20:52 +0900

    Le premier commit
    1. 기본 세팅
    - git
    - npm
    - prettierc
    - kakao API 키 얻어서 html 에서 로드

---
+++ src/controller.js
@@ -0,0 +1,10 @@
+// import { async } from 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+
+class Controller {
+    static init() {
+        console.log('hi!');
+    }
+};
+
+Controller.init();
---
+++ src/test.js
@@ -0,0 +1,4 @@
+// 지도를 담을 영역의 DOM ref
+const container = document.getElementById('map'); 
+// 지도를 생성할 떄 필요한 기본옵션
+const options = {}
