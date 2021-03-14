0acc24ad9efe078fed255b229e59be9f71aa9429
yoonsang lee <highp0912@protonmail.ch>
2021-03-14 22:13:28 +0900

    title toggle finish

--- public/src/main-title/main-title.js
+++ public/src/main-title/main-title.js
@@ -1,9 +1,10 @@
 class MainTitle {
   _mainTitleEl = document.querySelector(".main-title");
   _toggleBtnEl = document.querySelector(".main-title-toggle");
+  _arrowEl = document.querySelector(".main-title-toggle").querySelector(".fas");
+  _isToggled = false;
 
-  constructor() {
-    console.log(this._toggleBtnEl);
+  constructor() {    
     this._toggleBtnEl.addEventListener(
       "click",
       this._onToggleMainTitle.bind(this)
@@ -12,6 +13,15 @@ class MainTitle {
 
   _onToggleMainTitle() {
     this._mainTitleEl.classList.toggle("hidden");
+    if (this._isToggled) {
+      this._isToggled = false;
+      this._arrowEl.classList.remove("fa-arrow-down");
+      this._arrowEl.classList.add("fa-arrow-up");
+    } else {
+      this._isToggled = true;
+      this._arrowEl.classList.add("fa-arrow-down");
+      this._arrowEl.classList.remove("fa-arrow-up");
+    }
   }
 }
 
